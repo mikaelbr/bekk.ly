d1e907ae1f1043ff8442f90b4de0518db3a7fb08
Ole-Martin Mørk <olemartin@gmail.com>
2014-06-25 15:54:12 +0200

    Added register and result html

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+var redis = require('./lib/redis');
+var cons = require('consolidate');
+var Handlebars = require('handlebars');
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded())
+
+app.engine('html', cons.handlebars);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.render('index');
+});
+
+app.post('/register', function(req, res) {
+  var urlOriginal = req.param('url');
+  console.log(urlOriginal);
+  
+  res.render('registered', {
+    "url":"http://bekk.ly/r/oerijg", 
+    "urlOriginal":urlOriginal
+  });
 });
 
 app.get('/redirect', function(req, res){
