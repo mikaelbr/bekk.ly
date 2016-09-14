4ace2bcddd8d605fbb37a86299a40fb681e01e37
Eivind Bergstøl <eivind@bergstol.no>
2016-09-14 20:45:07 +0200

    Dratt inn BEKK-profil ihht. Design guidelines. Oppdatert noen libber og fått litt bedre funksjon.

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 var express = require('express');
 var app = express();
 var redis = require('./lib/redis');
-var cons = require('consolidate');
-var Handlebars = require('handlebars');
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded())
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var fs = require("fs");
+app.use(bodyParser.urlencoded());
 
-app.engine('html', cons.handlebars);
+
+app.engine('html', exphbs.create({extname: '.html'}).engine);
 app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.use('/assets', express.static('assets'));
 
-
-app.get('/', function(req, res){
-  res.render('index');
+app.get('/', function (req, res) {
+    res.render('index', {"body": "register"});
 });
 
-app.post('/register', function(req, res) {
-  var urlOriginal = req.param('url');
-  console.log(urlOriginal);
-  
-  var result = redis.shortenUrl(urlOriginal);
+app.post('/register', function (req, res) {
+    var urlOriginal = req.param('url');
+    console.log("Shorten:" + urlOriginal);
+
+    var result = redis.shortenUrl(urlOriginal);
     console.log("Result: " + result);
-  
-    res.render('registered', {
-      "url":"http://localhost:3000/r/" + result, 
-      "urlOriginal":urlOriginal
+
+    res.render('index', {
+        "body": "registered",
+        "url": req.protocol + '://' + req.get('host') + "/r/" + result,
+        "urlOriginal": urlOriginal
     });
-  
+
 });
 
-app.get('/r/:key', function(req, res) {
-  var key = req.param("key");
-  console.log(key)
-  redis.getUrl(key, function(err, result) {
-      res.redirect(result);
-  });
+app.get('/r/:key', function (req, res) {
+    var key = req.param("key");
+    console.log(key);
+    redis.getUrl(key, function (err, result) {
+        res.redirect(result);
+    });
 });
 
 var port = Number(process.env.PORT || 3000);
